fdab4c06744a9f419b722bc06a621a2aeaca1099
Anna Pushkareva <smirnova_spb@mail.ru>
2024-09-15 15:26:13 +0300

    add react-icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { MdWbSunny } from 'react-icons/md';
+
 interface NavbarProps {}
 
 export const Navbar = (props: NavbarProps) => {
@@ -7,6 +9,7 @@ export const Navbar = (props: NavbarProps) => {
             <div className='h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto'>
                 <p className='flex items-center justify-center gap-2'>
                     <h2 className='text-gray-500 text-3xl'>Weather</h2>
+                    <MdWbSunny className='text-3xl mt-1 text-yellow-300' />
                 </p>
             </div>
         </nav>
