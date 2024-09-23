ca12cbc2271b59cf92646500c732875038c2eb2b
Anna Pushkareva <smirnova_spb@mail.ru>
2024-09-23 21:27:15 +0300

    add react-query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+/** @format */
+'use client';
+
 import { Navbar } from '@/components/Navbar';
+import { useQuery } from 'react-query';
+
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=2`;
 
 export default function Home() {
-    // https://api.openweathermap.org/data/3.0/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56
+    const { isLoading, data } = useQuery('repoData', () =>
+        fetch(WEATHER_URL).then((res) => res.json())
+    );
+    console.log({ data });
+
+    if (isLoading) return 'Loading...';
+
     return (
         <div className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
             <Navbar />
