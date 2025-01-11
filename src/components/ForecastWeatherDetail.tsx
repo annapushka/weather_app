26a4ede2da0253cf4e631a99a01efbd09a0ae28d
Anna Pushkareva <smirnova_spb@mail.ru>
2025-01-11 13:12:31 +0300

    add WeatherIcon to ForecastWeatherDetail

--- src/components/ForecastWeatherDetail.tsx
+++ src/components/ForecastWeatherDetail.tsx
@@ -1,5 +1,16 @@
 import { Container } from './Container';
+import { WeatherIcon } from './WeatherIcon';
 
-export const ForecastWeatherDetail = () => {
-    return <Container></Container>;
+interface ForecastWeatherDetailProps {}
+
+export const ForecastWeatherDetail = ({}: ForecastWeatherDetailProps) => {
+    return (
+        <Container className='gap-4'>
+            <section className='flex gap-4 items-center px-4'>
+                <div>
+                    <WeatherIcon iconName={''} />
+                </div>
+            </section>
+        </Container>
+    );
 };
